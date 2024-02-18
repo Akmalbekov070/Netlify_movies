a63bdb72442d6965468cab950bb38a935e0b2454
Akmalbekov070 <akmalbekov2022@gmail.com>
2024-02-18 22:26:37 +0300

    clerk/nextjs upload now

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,12 +1,15 @@
 import Header from '@/components/Header/Header';
 import '@/styles/globals.css';
 import { ChakraProvider } from '@chakra-ui/react';
+import { ClerkProvider } from 'clerk/nextjs';
 
 export default function App({ Component, pageProps }) {
 	return (
-		<ChakraProvider>
-			<Header />
-			<Component {...pageProps} />
-		</ChakraProvider>
+		<ClerkProvider>
+			<ChakraProvider>
+				<Header />
+				<Component {...pageProps} />
+			</ChakraProvider>
+		</ClerkProvider>
 	);
 }
