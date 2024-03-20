d9b1f4d1f24f64eb05c3c5613032fcb3ef8e48db
Akmalbekov070 <akmalbekov2022@gmail.com>
2024-03-20 17:22:14 +0300

    random api success

--- src/page/cinemaPage/cinema.js
+++ src/page/cinemaPage/cinema.js
@@ -7,4 +7,12 @@ export const AllCinema = [
 		image:
 			'https://cdnb.artstation.com/p/assets/images/images/073/540/187/large/adit-15-kfp-homemaincarousel-1920x1025.jpg?1709891270',
 	},
+	{
+		id: 2,
+		Name: 'Ali',
+		tittle:
+			"After three death-defying adventures defeating world-class villains with his unmatched courage and mad martial arts skills, Po, the Dragon Warrior (Golden Globe nominee Jack Black), is called upon by destiny to... give it a rest already. More specifically, he's tapped to become the Spiritual Leader of the Valley of Peace.",
+		image:
+			'https://cdnb.artstation.com/p/assets/images/images/073/540/187/large/adit-15-kfp-homemaincarousel-1920x1025.jpg?1709891270',
+	},
 ];
--- src/page/cinemaPage/cinemaHome.jsx
+++ src/page/cinemaPage/cinemaHome.jsx
@@ -1,35 +1,36 @@
 import MenuPage from '@/components/menu/menu';
 import { Box, Button, Heading, Image, Text } from '@chakra-ui/react';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { AllCinema } from '../cinemaPage/cinema';
+import axios from 'axios';
 
 export default function CinemaHome() {
-	console.log(AllCinema);
+	const movies = AllCinema[Math.floor(Math.random() * AllCinema.length)];
+
+	console.log(movies);
 	return (
 		<>
-			{AllCinema.map(item => (
-				<Box w={'full'} position={'relative'} key={item.id}>
-					<Image w={'full'} h={'100vh'} objectFit={'cover'} src={item.image} title='Cinima page image' alt='Cinima page image' />
-					<MenuPage />
-					<Box w={'full'} position={'absolute'} top={0} left={0} zIndex={20}>
-						<Box
-							w={'full'}
-							pt={{ base: '150px', lg: '190px', xl: '200px' }}
-							px={{ base: '30px', sm: '120px', lg: '150px', xl: '250px' }}
-							color={'white'}
-						>
-							<Heading fontSize={{ base: '2xl', sm: '3xl', lg: '4xl', xl: '5xl' }} fontFamily={'-moz-initial'}>
-								{item.Name}
-							</Heading>
-							<Text w={{ xl: '80%' }} fontSize={'lg'} py={6} fontWeight={'500'}>
-								{item.tittle}
-							</Text>
-							<Button>Watching a movie</Button>
-						</Box>
+			<Box w={'full'} position={'relative'} key={movies.id}>
+				<Image w={'full'} h={'100vh'} objectFit={'cover'} src={movies.image} title='Cinima page image' alt='Cinima page image' />
+				<MenuPage />
+				<Box w={'full'} position={'absolute'} top={0} left={0} zIndex={20}>
+					<Box
+						w={'full'}
+						pt={{ base: '150px', lg: '190px', xl: '200px' }}
+						px={{ base: '30px', sm: '120px', lg: '150px', xl: '250px' }}
+						color={'white'}
+					>
+						<Heading fontSize={{ base: '2xl', sm: '3xl', lg: '4xl', xl: '5xl' }} fontFamily={'-moz-initial'}>
+							{movies.Name}
+						</Heading>
+						<Text w={{ xl: '80%' }} fontSize={'lg'} py={6} fontWeight={'500'}>
+							{movies.tittle}
+						</Text>
+						<Button>Watching a movie</Button>
 					</Box>
-					<Box w={'full'} h={'100vh'} position={'absolute'} bg={'blackAlpha.700'} top={0} left={0}></Box>
 				</Box>
-			))}
+				<Box w={'full'} h={'100vh'} position={'absolute'} bg={'blackAlpha.700'} top={0} left={0}></Box>
+			</Box>
 		</>
 	);
 }
