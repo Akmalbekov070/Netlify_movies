24fb24ce2efade129f5a2076b9dd033cbf16c5d6
Akmalbekov070 <akmalbekov2022@gmail.com>
2024-03-20 21:33:40 +0300

    API all sucsess upload

--- src/page/cinemaPage/cinema.js
+++ src/page/cinemaPage/cinema.js
@@ -9,9 +9,58 @@ export const AllCinema = [
 	},
 	{
 		id: 2,
-		Name: 'Mega Miya',
+		Name: 'MegaMind',
 		tittle:
 			'Megamind is the most brilliant and unluckiest villain in the world. For many years now, he has been trying to conquer Metro City in a variety of ways. But every such attempt ends in failure due to the fault of a superhero named Metro-Man. But the villain kills the superhero, and suddenly Megamind loses his purpose in life. Supervillain without a superhero',
 		image: 'https://million-wallpapers.ru/wallpapers/4/5/432405258107820/multfilm-mega-mi-zh-ne-barly-kej-pkerler.jpg',
+	},
+	{
+		id: 3,
+		Name: 'Aquaman',
+		tittle:
+			'The film takes place in the vast and exciting underwater world of the Seven Seas, and the plot introduces the audience to the emergence and important events of the life of half-eastern quarterback Arthur Curry - not only to put himself in the face, but also to helps determine eligibility. those meant for him ... the king!',
+		image: 'https://w.forfun.com/fetch/5a/5aa7ce56d1990676aee85b7010423fd3.jpeg',
+	},
+	{
+		id: 4,
+		Name: 'cat in boots',
+		tittle:
+			"Swords cross and hearts break in adventures with one of Shrek's favorite characters, Puss in Boots. It's a long journey from Puss in Boots's early years, when he teams up with clever Humpty Dumpty and inventive Kitty to steal the famous egg geese.",
+		image: 'https://img.razrisyika.ru/kart/86/1200/340250-kisa-myagkolapka-29.jpg',
+	},
+	{
+		id: 5,
+		Name: 'The avengers',
+		tittle:
+			"Description: Thor's brother Loki is back, and this time he's not alone. Earth is on the brink of slavery and only the best of the best can save humanity. S.I.T. Nick Fury, the head of an international organization, gathers famous fighters for justice and goodness to repel the attack. Led by Captain America, Iron Man, Thor, The Incredible Hulk, Hawkeye and Black Widow go to war with the invader.",
+		image: 'https://topdeck.ru/uploads/monthly_2019_04/Avengers_Age_of_Ultron_479056.jpg.744eadd702454e4080d85c15a1eb87b8.jpg',
+	},
+	{
+		id: 6,
+		Name: 'The avengers 3',
+		tittle:
+			'While the Avengers and their allies continue to protect the world with a variety of villains, one hero cannot fight a new threat from outer space: Thanos. An intergalactic tyrant is aiming to collect all six stones. It is an artifact of incredible power with which it can alter reality. All of this escalating event has occurred in the present. The fate of the land is never in doubt.',
+		image: 'https://zolotoy.ru/wp-content/uploads/2018/12/%D0%BC%D1%81%D1%82%D0%B8%D1%82%D0%B5%D0%BB%D0%B83.jpg',
+	},
+	{
+		id: 7,
+		Name: 'The avengers 4',
+		tittle:
+			"The surviving members of the Avengers and their allies must devise a new plan to counter the ravages of the mighty titan Thanos. After the biggest and most tragic battle in history, they can't be wrong.",
+		image: 'https://images.hdqwalls.com/download/avengers-endgame-come-together-pi-2880x1800.jpg',
+	},
+	{
+		id: 8,
+		Name: 'Sonic',
+		tittle:
+			'Sonic, the bright blue hedgehog, and his new friend, human best friend Tom Wachowski, are introduced to the complexities of life on Earth and a villain who wants to capture Sonic and use his unlimited superpowers to rule the world. the doctor encounters Robotnik.',
+		image: 'https://w.forfun.com/fetch/e2/e2306656305ad7f0aee2752d8d1e443f.jpeg?w=1470&r=0.5625',
+	},
+	{
+		id: 9,
+		Name: 'Sonic 2',
+		tittle:
+			"Yashil tepalikka joylashib, Sonic haqiqiy qahramonning barcha imkoniyatlariga ega ekanligini isbotlashga intiladi. Qahramonlik sinovi esa uzoq kutilmaydi: yovuz doktor Robotnik yana fitna uyushtirmoqda. Bu safar - sirli hamkor Knuckles bilan. Ular birgalikda butun tsivilizatsiyalarni yo'q qilishga qodir kuchlarni o'z ichiga olgan bebaho zumradni qidiradilar. Sonic Tails tulkisi bilan birgalikda dunyo bo'ylab sayohat qiladi va zumradni noto'g'ri qo'llarga tushishidan oldin topadi.",
+		image: 'https://st32.stpulscen.ru/images/product/477/140/332_big.jpeg',
 	},
 ];
