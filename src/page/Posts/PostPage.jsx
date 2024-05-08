168824e0fc6a5ec0e482fc6da23aba4787a0c4c2
Akmalbekov070 <akmalbekov2022@gmail.com>
2024-05-08 11:55:03 +0300

    upload new page design

--- src/page/Posts/PostPage.jsx
+++ src/page/Posts/PostPage.jsx
@@ -1,5 +1,19 @@
+import MenuPage from '@/components/menu/menu';
+import { Box, Image } from '@chakra-ui/react';
 import React from 'react';
 
 export default function PostPage() {
-	return <div>PostPage</div>;
+	return (
+		<>
+			<MenuPage />
+			<Box>
+				<Image
+					w={'full'}
+					h={'100vh'}
+					src='https://luxe-host.ru/wp-content/uploads/f/8/5/f853048277ea372e0c0bab8a4bed7e9f.gif'
+					alt='Loading '
+				/>
+			</Box>
+		</>
+	);
 }
